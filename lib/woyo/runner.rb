694b58c7df90838e71ab941bcfeb3c416934497f
Gerard Fowley <gerard.fowley@iqeo.net>
2014-04-28 18:30:12 -0400

    shell command: woyo new

--- lib/woyo/runner.rb
+++ lib/woyo/runner.rb
@@ -6,29 +6,91 @@ class Runner
 
   def self.run args, out: $stdout, err: $stderr
 
-    if args.empty? || args.include?('-h') || args.include?('--help')
-      print_help err
+    @args = args.dup
+    @out = out
+    @err = err
+
+    code = case @args.first
+      when 'new'     then mode_new
+      when 'server'  then mode_server
+      when 'console' then mode_console
+      end
+    return code if code
+      
+    if @args.empty? || @args.include?('-h') || @args.include?('--help')
+      print_help
       return 0
     end
 
-    if args.include?('-v') || args.include?('--version')
-      print_version err
+    if @args.include?('-v') || @args.include?('--version')
+      print_version
       return 0
     end
 
   end
 
-  def self.print_help io
-    io.puts "Usage: woyo..."
-    io.puts
-    io.puts "............."
-    io.puts "............."
-    io.puts "............."
+  def self.fail msg, code
+    print_error msg
+    return code
   end
 
-  def self.print_version io
-    io.puts "woyo server version #{Woyo::SERVER_VERSION}"
-    io.puts "woyo world  version #{Woyo::WORLD_VERSION}"
+  def self.mode_new
+    if @args.include?('-h') || @args.include?('--help')
+      print_help_new
+      return 0
+    end
+    mode, dir = @args.shift 2
+    if dir.nil?
+      print_error 'No directory provided'
+      return -1
+    end
+    if Dir.exists? dir
+      unless @args.include?('-f') || @args.include?('--force')
+        print_error 'Directory already exists'
+        return -2 
+      end
+    end
+    if File.exists? dir
+      unless @args.include?('-f') || @args.include?('--force')
+        print_error 'File exists with same name'
+        return -3 
+      end
+    end
+    [ 'public', 'views', 'world' ].each do |subdir|
+      FileUtils.mkdir_p File.join( dir, subdir )
+    end
+    return 0
+  end
+
+  def self.mode_server
+  end
+
+  def self.mode_console
+  end
+
+  def self.print_help
+    @err.puts "Usage: woyo ..."
+    @err.puts
+    @err.puts "............."
+    @err.puts "............."
+    @err.puts "............."
+  end
+
+  def self.print_help_new
+    @err.puts "Usage: woyo new ..."
+    @err.puts
+    @err.puts "............."
+    @err.puts "............."
+    @err.puts "............."
+  end
+
+  def self.print_error msg
+    @err.puts "Error: #{msg}"
+  end
+
+  def self.print_version
+    @err.puts "woyo server version #{Woyo::SERVER_VERSION}"
+    @err.puts "woyo world  version #{Woyo::WORLD_VERSION}"
   end
 
 end
--- spec/woyo/server/runner_spec.rb
+++ spec/woyo/server/runner_spec.rb
@@ -1,11 +1,29 @@
 require 'woyo/runner'
+require 'fileutils'
 require 'stringio'
 
 describe Woyo::Runner do
 
+  before :all do
+    @original_path = Dir.pwd
+    File.basename(@original_path).should eq 'woyo-server'
+    @test_dir = 'tmp/test'
+    @expected_entries = [ '.', '..', 'public', 'views', 'world' ]
+  end
+
   before :each do
+    Dir.pwd.should eq @original_path
+    FileUtils.rm_rf @test_dir
+    FileUtils.mkdir_p @test_dir
+    Dir.chdir @test_dir
+    Dir.pwd.should eq File.join( @original_path, @test_dir )
     @output = StringIO.new
-    @error  = StringIO.new
+    @error  = StringIO.new                 
+  end
+
+  after :each do
+    Dir.chdir @original_path
+    Dir.pwd.should eq @original_path
   end
 
   it 'prints a helpful message to stderr for help (-h/--help) switch' do
@@ -24,10 +42,48 @@ describe Woyo::Runner do
     end
   end
 
-  it 'starts a world application server'
+  it 'new <dir> command creates a world application directory' do
+    [['new','testworld'],['new','test/testworld']].each do |args|
+      Woyo::Runner.run( args, out: @output, err: @error ).should eq 0
+      Dir.should exist args[1]
+      Dir.entries(args[1]).sort.should eq @expected_entries
+    end
+  end
 
-  it 'creates a world application directory'
+  it 'new command requires a directory be specified' do
+    Woyo::Runner.run( ['new'], out: @output, err: @error ).should eq -1
+  end
 
-  it 'prints help'
+  it 'new <dir> requires force (-f/--force) for existing directory' do
+    [['new','testworld'],['new','test/testworld'],['new','.']].each do |args|
+      dir = args[1]
+      FileUtils.mkdir_p dir
+      Dir.should exist dir
+      Woyo::Runner.run( args, out: @output, err: @error ).should eq -2
+      [['--force'],['-f']].each do |force|
+        FileUtils.mkdir_p dir
+        Dir.should exist dir
+        Woyo::Runner.run( args + force, out: @output, err: @error ).should eq 0
+        Dir.should exist dir
+        (Dir.entries(dir) & @expected_entries).sort.should eq @expected_entries      # subset
+        FileUtils.rm_rf dir
+      end
+    end
+  end
+
+  it 'new command + help (-h/--help) explains new command' do
+    [['-h'],['--help']].each do |help|
+      Woyo::Runner.run( ['new'] + help, out: @output, err: @error ).should eq 0
+      @error.string.should include 'woyo new'
+    end
+  end
+
+  it 'server command starts a world application server'
+
+  it 'server command + help (-h/--help) explains server command'
+
+  it 'console command starts a world application conosle'
+
+  it 'console command + help (-h/--help) explains console command'
 
 end
